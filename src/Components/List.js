d2ee8c16348c190de6cb01af03fc55eed7abded9
Shekhar-3010 <98082889+Shekhar-3010@users.noreply.github.com>
2023-04-27 15:41:12 +0530

    ADD:props started

---
+++ src/Components/List.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div>
+      <ul>
+      <li>html</li>
+      <li>css</li>
+      <li>javascript</li>
+      <li>react</li>
+      </ul>
+    </div>
+  )
+}
+
+export default List
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { add, subtract, multiply, divide } from "./calculator";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
+
+ReactDOM.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        
